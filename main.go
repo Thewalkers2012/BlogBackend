d215818d32b71a5cba681074e55dd0069730a800
thewalkers <1518832673@qq.com>
2021-08-27 02:40:37 +0800

    开始写，已经添加脚手架

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/Thewalkers2012/BlogBackend/logger"
+	"github.com/Thewalkers2012/BlogBackend/repository/mysql"
+	"github.com/Thewalkers2012/BlogBackend/repository/redis"
+	"github.com/Thewalkers2012/BlogBackend/routes"
+	"github.com/Thewalkers2012/BlogBackend/settings"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+)
+
+// Go Web 开发较通用的脚手架模版
+
+func main() {
+	// 1. 加载配置文件
+	if err := settings.Init(); err != nil {
+		fmt.Printf("init settings failed, err: %v\n", err)
+		return
+	}
+
+	// 2. 初始化日志
+	if err := logger.Init(settings.Config.LogConfig); err != nil {
+		fmt.Printf("init logger failed, err: %v\n", err)
+		return
+	}
+	defer zap.L().Sync()
+	zap.L().Debug("logger init success ...")
+
+	// 3. 初始化 mysql 连接
+	if err := mysql.Init(settings.Config.MySQLConfig); err != nil {
+		fmt.Printf("init mysql failed, err: %v\n", err)
+		return
+	}
+	defer mysql.Close()
+
+	// 4. 初始化 redis 连接
+	if err := redis.Init(settings.Config.RedisConfig); err != nil {
+		fmt.Printf("init redis failed, err: %v\n", err)
+		return
+	}
+
+	// 5. 注册路由
+	r := routes.Setup()
+
+	// 6. 启动服务 （优雅关机操作）
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%d", viper.GetInt("app.port")),
+		Handler: r,
+	}
+
+	go func() {
+		// 开启一个 goroutine 启动服务
+		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s", err.Error())
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+
+	zap.L().Info("Shutdown Server ...")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		zap.L().Fatal("Server Shutdown: ", zap.Error(err))
+	}
+
+	zap.L().Info("Server exiting")
+
+}
---
+++ repository/mysql/mysql.go
@@ -0,0 +1,36 @@
+package mysql
+
+import (
+	"fmt"
+
+	"github.com/Thewalkers2012/BlogBackend/settings"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+	"go.uber.org/zap"
+)
+
+var db *sqlx.DB
+
+func Init(cfg *settings.MySQLConfig) (err error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True",
+		cfg.User,
+		cfg.Password,
+		cfg.Host,
+		cfg.Port,
+		cfg.DBName,
+	)
+	// 也可以使用 MustConnect 链接不成功就 panic
+	db, err = sqlx.Connect("mysql", dsn)
+	if err != nil {
+		zap.L().Error("connect DB failed", zap.Error(err))
+		return
+	}
+	db.SetMaxOpenConns(cfg.MaxOpenConns)
+	db.SetMaxIdleConns(cfg.MaxIdleConns)
+
+	return
+}
+
+func Close() {
+	_ = db.Close()
+}
